28b03a0c16f29991b7335b966585de12b1fccbfa
SK ANUBHAB HOSSAIN <116865734+Sk-Anubhab-Hossain@users.noreply.github.com>
2025-03-17 11:44:51 +0530

    [3] [Time Beats: 13.33%] [Memory Beats: 9.18%] - LeetPush

---
+++ Longest-Substring-Without-Repeating-Characters.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        set<char> Set;
+        int max=0;
+        int start=0, end=0;
+        while(start<s.size()){
+            auto it=Set.find(s[start]);
+            if(it == Set.end()){
+                Set.insert(s[start]);
+                if((start-end+1)>max){
+                    max=start-end+1;
+                }
+                start++;
+            }
+            else{
+                Set.erase(s[end]);
+                end++;
+            }
+            
+        }
+
+
+        return max;
+
+    }
+};
