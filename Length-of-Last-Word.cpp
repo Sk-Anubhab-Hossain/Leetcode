40253ea78de44d253dfe324a5192f150262c0888
SK ANUBHAB HOSSAIN <116865734+Sk-Anubhab-Hossain@users.noreply.github.com>
2025-03-29 19:54:54 +0530

    [58] [Time Beats: 100.00%] [Memory Beats: 90.55%] - LeetPush

---
+++ Length-of-Last-Word.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length=0;
+        int n=s.size()-1;
+        while(n>=0 && s[n]==' '){
+            n--;
+        }
+        while(n>=0 && s[n]!=' '){
+            length++;
+            n--;
+        }
+        return length;
+    }
+};
