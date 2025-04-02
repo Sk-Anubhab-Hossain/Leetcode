2a134c2f64aec3d8a3096fd0866d4eee13709ec6
SK ANUBHAB HOSSAIN <116865734+Sk-Anubhab-Hossain@users.noreply.github.com>
2025-04-02 10:43:53 +0530

    [144] [Time Beats: 100.00%] [Memory Beats: 43.20%] - LeetPush

--- Binary-Tree-Preorder-Traversal.cpp
+++ Binary-Tree-Preorder-Traversal.cpp
@@ -15,12 +15,29 @@ public:
         vector<int> result;
         preorderHelper(root,result);
         return result;
+
     }
 private:
     void preorderHelper(TreeNode* root, vector<int>& result){
         if(root==nullptr) return;
-        result.push_back(root->val);
-        preorderHelper(root->left,result);
-        preorderHelper(root->right,result);
+    //     result.push_back(root->val);
+    //     preorderHelper(root->left,result);
+    //     preorderHelper(root->right,result);
+
+        stack<TreeNode*> st;
+        st.push(root);
+        while(!st.empty()){
+            TreeNode* node=st.top();
+            st.pop();
+            result.push_back(node->val);
+
+            if(node->right){
+                st.push(node->right);
+            }
+            if(node->left){
+                st.push(node->left);
+            }
+
+        }
     }
 };
