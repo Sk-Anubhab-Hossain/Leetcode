5b70670a887da21dca6b2eb19f76ff50efcc7fdf
SK ANUBHAB HOSSAIN <116865734+Sk-Anubhab-Hossain@users.noreply.github.com>
2025-04-07 23:48:51 +0530

    [2769] [Time Beats: 14.59%] [Memory Beats: 43.59%] - LeetPush

---
+++ Find-the-Maximum-Achievable-Number.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int theMaximumAchievableX(int num, int t) {
+        // for(int i=0;i<t;i++){
+        //     num++;
+        // }
+        // return num+t;
+        return num+(2*t);
+    }
+};
